acaefa8cdcd90632bc37afb2997f821e6b8a0add
Jonatan Dalbosco <48532656+jonatandalbosco@users.noreply.github.com>
2022-04-04 08:51:05 -0300

    Add logo

--- js/main.js
+++ js/main.js
@@ -146,7 +146,7 @@ $(document).ready(function(){
       // -------   Mail Send ajax
 
          $(document).ready(function() {
-            var form = $('#booking'); // contact form
+            var form = $('#booking'); // Contato form
             var submit = $('.submit-btn'); // submit button
             var alert = $('.alert-msg'); // alert div for show alert message
 
