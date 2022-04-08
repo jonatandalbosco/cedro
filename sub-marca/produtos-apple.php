0ae9ab854c5942a31bb2c70b22bd6e4f4e3e01cd
Valmir da Silva da Silveira <48221291+valmirdasilva@users.noreply.github.com>
2022-04-07 21:36:13 -0300

    inicio page produtos marca

---
+++ sub-marca/produtos-apple.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Shop || RepairPlus || Responsive HTML 5 Template</title>
+
+    <!-- responsive meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- For IE -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- master stylesheet -->
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- Responsive stylesheet -->
+    <link rel="stylesheet" href="../css/responsive.css">
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="../images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="../images/favicon/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="../images/favicon/favicon-16x16.png" sizes="16x16">
+
+    <!-- Fixing Internet Explorer-->
+    <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <script src="js/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <!--Start Preloader -->
+    <div class="preloader"></div>
+    <!--End Preloader -->
+
+    <?php include ("../includes/nav.php");?>
+
+    <!--Start breadcrumb area-->
+    <section class="breadcrumb-area">
+        <div class="container text-center">
+            <h1>Marca do produto que deseja</h1>
+        </div>
+    </section>
+    <!--End breadcrumb area-->
+
+    <!--Start breadcrumb bottom area-->
+    <section class="breadcrumb-botton-area">
+        <div class="container">
+            <div class="left pull-left">
+                <ul>
+                    <li><a href="../index.php">Inicio</a></li>
+                    <li><i class="fa fa-caret-right" aria-hidden="true"></i></li>
+                    <li>Marca do produto que deseja</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <!--End breadcrumb bottom area-->
+
+    <!--Start blog area-->
+    <section id="shop-area" class="main-shop-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="shop-content">
+                        <div class="row">
+
+
+                            <!--Start single product item-->
+                            <div class="col-md-4 col-sm-4 col-xs-12">
+                                <div class="single-product-item">
+                                    <div class="img-holder">
+                                        <img src="../images/marca-produtos/apple-logo.jpg" alt="Awesome Product Image">
+                                        <div class="overlay-box">
+                                            <div class="box">
+                                                <div class="content">
+                                                    <a class="thm-btn bg-1" href="shop-single.html">Ver agora</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="title-holder">
+                                        <div class="top clearfix">
+                                            <div class="product-title text-center">
+                                                <a href="produt">
+                                                    <h5>APPLE</h5>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!--End single product item-->
+
+                            <!--Start single product item-->
+                            <div class="col-md-4 col-sm-4 col-xs-12">
+                                <div class="single-product-item">
+                                    <div class="img-holder">
+                                        <img src="../images/marca-produtos/logo-samsung.png" alt="Awesome Product Image">
+                                        <div class="overlay-box">
+                                            <div class="box">
+                                                <div class="content">
+                                                    <a class="thm-btn bg-1" href="shop-single.html">Ver agora</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="title-holder">
+                                        <div class="top clearfix">
+                                            <div class="product-title text-center">
+                                                <a href="shop-single.html">
+                                                    <h5>SAMSUNG</h5>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!--End single product item-->
+
+                            <!--Start single product item-->
+                            <div class="col-md-4 col-sm-4 col-xs-12">
+                                <div class="single-product-item">
+                                    <div class="img-holder">
+                                        <img src="../images/marca-produtos/logo-motorola-white.png"
+                                            alt="Awesome Product Image">
+                                        <div class="overlay-box">
+                                            <div class="box">
+                                                <div class="content">
+                                                    <a class="thm-btn bg-1" href="shop-single.html">Ver agora</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="title-holder">
+                                        <div class="top clearfix">
+                                            <div class="product-title text-center">
+                                                <a href="shop-single.html">
+                                                    <h5>MOTOROLA</h5>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!--End single product item-->
+
+                            <!--Start single product item-->
+                            <div class="col-md-4 col-sm-4 col-xs-12">
+                                <div class="single-product-item">
+                                    <div class="img-holder">
+                                        <img src="../images/marca-produtos/logo-asus-white.png"
+                                            alt="Awesome Product Image">
+                                        <div class="overlay-box">
+                                            <div class="box">
+                                                <div class="content">
+                                                    <a class="thm-btn bg-1" href="shop-single.html">Ver agora</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="title-holder">
+                                        <div class="top clearfix">
+                                            <div class="product-title text-center">
+                                                <a href="shop-single.html">
+                                                    <h5>ASUS</h5>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!--End single product item-->
+
+                            <!--Start single product item-->
+                            <div class="col-md-4 col-sm-4 col-xs-12">
+                                <div class="single-product-item">
+                                    <div class="img-holder">
+                                        <img src="../images/marca-produtos/xiamo-logo-white.png"
+                                            alt="Awesome Product Image">
+                                        <div class="overlay-box">
+                                            <div class="box">
+                                                <div class="content">
+                                                    <a class="thm-btn bg-1" href="shop-single.html">Ver agora</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="title-holder">
+                                        <div class="top clearfix">
+                                            <div class="product-title text-center">
+                                                <a href="shop-single.html">
+                                                    <h5>XIAOMI</h5>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!--End single product item-->
+
+                            
+                            <!--Start single product item-->
+                            <div class="col-md-4 col-sm-4 col-xs-12">
+                                <div class="single-product-item">
+                                    <div class="img-holder">
+                                        <img src="../images/marca-produtos/logo-lg-white.png"
+                                            alt="Awesome Product Image">
+                                        <div class="overlay-box">
+                                            <div class="box">
+                                                <div class="content">
+                                                    <a class="thm-btn bg-1" href="shop-single.html">Ver agora</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="title-holder">
+                                        <div class="top clearfix">
+                                            <div class="product-title text-center">
+                                                <a href="shop-single.html">
+                                                    <h5>LG</h5>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!--End single product item-->
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+            <!--End Sidebar Wrapper-->
+        </div>
+        </div>
+    </section>
+    <!--End blog area-->
+
+
+    <?php include ("../includes/footer.php");?>
+
+
+    <!--Scroll to top-->
+    <div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-angle-up"></span></div>
+
+    <!-- main jQuery -->
+    <script src="../js/jquery.js"></script>
+    <!-- bootstrap -->
+    <script src="../js/bootstrap.min.js"></script>
+    <!-- bx slider -->
+    <script src="../js/jquery.bxslider.min.js"></script>
+    <!-- count to -->
+    <script src="../js/jquery.countTo.js"></script>
+    <!-- owl carousel -->
+    <script src="../js/owl.carousel.min.js"></script>
+    <!-- validate -->
+    <script src="../js/validate.js"></script>
+    <!-- mixit up -->
+    <script src="../js/jquery.mixitup.min.js"></script>
+    <!-- easing -->
+    <script src="../js/jquery.easing.min.js"></script>
+    <!-- gmap helper -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAHzPSV2jshbjI8fqnC_C4L08ffnj5EN3A"></script>
+    <!--gmap script-->
+    <script src="../js/gmaps.js"></script>
+    <script src="../js/map-helper.js"></script>
+    <!-- video responsive script -->
+    <script src="../js/jquery.fitvids.js"></script>
+    <!-- jQuery ui js -->
+    <script src="../assets/jquery-ui-1.11.4/jquery-ui.js"></script>
+    <!-- Language Switche  -->
+    <script src="../assets/language-switcher/jquery.polyglot.language.switcher.js"></script>
+    <!-- fancy box -->
+    <script src="../js/jquery.fancybox.pack.js"></script>
+    <script src="../js/jquery.appear.js"></script>
+    <!-- isotope script-->
+    <script src="../js/isotope.js"></script>
+    <script src="../js/jquery.prettyPhoto.js"></script>
+    <!-- jQuery timepicker js -->
+    <script src="../assets/timepicker/timePicker.js"></script>
+    <!-- Bootstrap select picker js -->
+    <script src="../assets/bootstrap-sl-1.12.1/bootstrap-select.js"></script>
+    <!-- Price filter  js -->
+    <script src="../assets/price-filter/nouislider.js"></script>
+
+
+    <!-- revolution slider js -->
+    <script src="../assets/revolution/js/jquery.themepunch.tools.min.js"></script>
+    <script src="../assets/revolution/js/jquery.themepunch.revolution.min.js"></script>
+    <script src="../assets/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+    <script src="../assets/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+    <script src="../assets/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+    <script src="../assets/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script src="../assets/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+    <script src="../assets/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script src="../assets/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+    <script src="../assets/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script src="../assets/revolution/js/extensions/revolution.extension.video.min.js"></script>
+
+
+
+    <!-- thm custom script -->
+    <script src="../js/custom.js"></script>
+
+
+
+
+
+
+</body>
+
+</html>
